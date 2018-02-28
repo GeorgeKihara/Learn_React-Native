dd8ca4ac6b1fb274350ad4625ebd700ada73bfa3
GeorgeKihara <gegohcomp@gmail.com>
2018-02-28 11:55:59 +0300

    basic comands

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [1,2,3]
+      todos: [1,2,3,5,6],
+      newTodo: ''
     }
   }
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({newTodo: value})
+  }
+  handlePress(){
+    this.setState({newTodo: "congrats"})
+  }
+  handlePress1(){
+    this.setState({newTodo: ''})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput/>
+        <TextInput value={this.state.newTodo} onChange={this.handleChange.bind(this)}></TextInput>
+        <TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>Click here</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.handlePress1.bind(this)}>
+          <Text>Reset</Text>
+        </TouchableHighlight>
         {this.state.todos.map(todo => <Text>{todo}</Text>)}
       </View>
     );
